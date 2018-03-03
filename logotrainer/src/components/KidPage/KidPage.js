8916409ee5a0daf4312eb1ff9a7adf4cc591725a
OlgaKuksa <firsty@tut.by>
2018-03-03 18:05:41 +0300

    added Menu to KidPage

--- logotrainer/src/components/KidPage/KidPage.js
+++ logotrainer/src/components/KidPage/KidPage.js
@@ -1,17 +1,56 @@
 import React, { Component } from "react";
-import { Icon, Button } from "semantic-ui-react";
+import { Icon, Button, Menu, Grid } from "semantic-ui-react";
 import { clearKidPage } from "../../actions/kidInPage";
 import { connect } from "react-redux";
+import "./KidPage.css";
 
 class KidPage extends Component {
+  state = {
+    activeItem: "sets"
+  };
+  handleItemClick = (ev, { name }) => {
+    this.setState({ activeItem: name });
+  };
   render() {
     return (
       <div>
-        <Button color="olive" onClick={this.props.clearKidPage}>
-          <Icon name="arrow left" />
-        </Button>
-        {this.props.kidInPage.firstName} {this.props.kidInPage.lastName} Info
-        about the child will be here
+        <Menu tabular color="olive" inverted>
+          <Menu.Item>
+            <Button color="olive" onClick={this.props.clearKidPage}>
+              <Icon name="arrow left" size="big" />
+            </Button>
+          </Menu.Item>
+          <Menu.Item
+            name="profile"
+            active={this.state.activeItem == "profile"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="user circle outline" />
+            Профиль
+          </Menu.Item>
+          <Menu.Item
+            name="sets"
+            active={this.state.activeItem == "sets"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="list ol" />
+            Комплексы упражнений
+          </Menu.Item>
+          <Menu.Item
+            name="progress"
+            active={this.state.activeItem == "progress"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="line chart" />
+            Динамика
+          </Menu.Item>
+          <div className="KidNameInMenu">
+            <Icon name="id card" />
+            {this.props.kidInPage.firstName +
+              " " +
+              this.props.kidInPage.lastName}
+          </div>
+        </Menu>
       </div>
     );
   }
