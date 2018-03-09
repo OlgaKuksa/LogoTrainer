cc082b9f37c9b9fa38308048d7fef00955d4eb31
OlgaKuksa <olga.kuksa@gmail.com>
2018-03-09 12:44:17 +0300

    Configuring server api

--- Server/Server/App_Start/RouteConfig.cs
+++ Server/Server/App_Start/RouteConfig.cs
@@ -10,10 +10,17 @@ namespace Server
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "DefaultApi",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { id = UrlParameter.Optional, action = "Index" },
+                namespaces: new[] { "Server.Controllers.Api, Server" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
             );
+            
         }
     }
 }
