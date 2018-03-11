44844d032c6847ba1e570762f690687f68adf3f1
OlgaKuksa <olga.kuksa@gmail.com>
2018-03-11 21:15:15 +0300

    Adding Model classes

---
+++ Server/Model/Operation/Group.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Logotrainer.Model.Operation
+{
+    public class Group
+    {
+        public Guid GroupId { get; set; }
+        public string GroupNumber { get; set; }
+    }
+}
---
+++ Server/Model/Operation/KidProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logotrainer.Model.Operation
+{
+    public class KidProfile
+    {
+        public Guid KidProfileId { get; set; }
+        public Guid KidId { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public IDictionary<Guid, Guid> TestResult { get; set; }
+    }
+}
