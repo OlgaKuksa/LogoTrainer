2c4c480e002f416d381de0deba0a4811b5d1ca51
OlgaKuksa <firsty@tut.by>
2018-03-12 00:33:35 +0300

    rechart hardcoded

--- logotrainer/src/components/KidPage/ProgressKid.js
+++ logotrainer/src/components/KidPage/ProgressKid.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Message, Icon } from "semantic-ui-react";
+import { BarChart, XAxis, YAxis, Bar, Tooltip, Legend } from "recharts";
 
 class ProgressKid extends Component {
   render() {
+    {
+      console.log(this.props.profiles);
+    }
     return (
       <div>
         {this.props.profiles.length < 3 ? (
@@ -13,14 +17,33 @@ class ProgressKid extends Component {
             профилей
           </Message>
         ) : (
-          <div>chart placeholder</div>
+          <div>
+            <BarChart width={500} height={300} data={this.props.profiles}>
+              <XAxis dataKey="profileDate" />
+              <YAxis />
+              <Legend />
+              <Tooltip />
+              <Bar dataKey="skillValue" fill="#8884d8" />
+            </BarChart>
+          </div>
         )}
       </div>
     );
   }
 }
 const mapStateToProps = state => ({
-  profiles: state.testResults
+  profiles: state.testResults.map(profile => ({
+    profileDate: profile.createDateTime,
+    skillValue: state.skills
+      .find(skillgroup => {
+        return (
+          skillgroup.skills.find(skill => skill.skillId == "11") != undefined
+        );
+      })
+      .skills.find(skill => skill.skillId == "11")
+      .skillLevels.find(level => level.levelId == profile.testResult["11"])
+      .levelNumber
+  }))
 });
 
 export default connect(mapStateToProps, undefined)(ProgressKid);
