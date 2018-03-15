399e13620286336f42edd0fdb10531c5ad1455cb
OlgaKuksa <olga.kuksa@gmail.com>
2018-03-15 06:45:38 +0300

    Add classes

--- Server/Server/Controllers/Api/KidSetController.cs
+++ Server/Server/Controllers/Api/KidSetController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Logotrainer.Model.Interfaces;
 using Logotrainer.Model.Operation;
@@ -28,9 +29,60 @@ namespace Logotrainer.Server.Controllers.Api
             //TODO
         }
 
+        [HttpPost] public IList<KidSet> FindByKid(Kid Kid)
+        {
+            throw new NotImplementedException();
+        }
+
         private IKidSetRepository KidSetRepository
         {
             get { return RepositoryFactory.CreateKidSetRepository(); }
+        }
+    }
+
+    [Authorize] public class ExerciseController : BaseRepositoryController
+    {
+        [HttpPost] public IList<Exercise> FindByKidSet(KidSet kidSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Exercise exercise)
+        {
+            
+        }
+
+        public void Update(Exercise exercise)
+        {
+            
+        }
+
+        public void Remove(Exercise exercise)
+        {
+            
+        }
+
+        public IList<Exercise> FindByFilter(ExerciseFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExerciseFilter
+    {
+        public Guid MainLevelId { get; set; }
+    }
+
+    public class KidProfileController : BaseRepositoryController
+    {
+        public void Add(KidProfile kidProfile)
+        {
+            
+        }
+
+        public IList<KidProfile> FindByKid(Kid kid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
